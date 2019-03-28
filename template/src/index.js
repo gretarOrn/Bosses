1f903c21ce703fd6e94ab8fb26dbdadb31ac8d87
Birkir K <birkir17@ru.is>
2019-03-28 15:09:51 +0000

    fixa fkn links

--- template/src/index.js
+++ template/src/index.js
@@ -1,37 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Route, Redirect } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import '../styles/site.css';
 import '../styles/site.less';
+import App from './components/App';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider, connect } from 'react-redux';
 import { getBosses } from './actions/BossActions';
 import reducers from './reducers';
-import Navbar from './components/Navbar/Navbar';
-import FrontPage from './components/FrontPage/FrontPage';
-import Bosses from './components/Bosses/Bosses';
-import Boss from './components/Boss/Boss';
 
-class Application extends React.Component {
-    componentDidMount() {
-      this.props.getBosses();
-    }
-    render() {
-      return (
-        <>
-            <Navbar/>
-            <Switch>
-                <Route exact path="/" component={ FrontPage }/>
-                <Route exact path="/bosses" component={ Bosses }/>
-                <Route path='/bosses/:id' component={ Boss } />
-            </Switch>
-        </>
-      );
-    }
-  }
-  
-  const AppWithRedux = connect(null, { getBosses })(Application);
+//getBosses();
+const AppWithRedux = connect(null, { getBosses })(App);
 
 ReactDOM.render(<Provider store={ createStore(reducers, applyMiddleware(thunk))}><Router><AppWithRedux /></Router></Provider>, document.getElementById('app'));
